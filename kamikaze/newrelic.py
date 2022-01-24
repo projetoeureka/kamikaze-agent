9a7be603b4aed368bf844890c5b1315e50c75745
Lucas Duailibe <dual@geekie.com.br>
2022-01-24 10:00:28 -0300

    Make the code compatible with Python 3

--- kamikaze/newrelic.py
+++ kamikaze/newrelic.py
@@ -1,19 +1,16 @@
-from __future__ import absolute_import
-
 import datetime
 import json
-import multiprocessing
+import logging
 import newrelic.agent
 import os
 import requests
-import traceback
-import time
 import timeit
 
+logger = logging.getLogger(__name__)
 
-@newrelic.agent.data_source_factory(name='Kamikaze CPU Usage')
+
+@newrelic.agent.data_source_factory(name="Kamikaze CPU Usage")
 class KamikazeDataSource(object):
-
     def __init__(self, settings, environ):
         self._timer = None
         self._times = None
@@ -28,18 +25,15 @@ class KamikazeDataSource(object):
         if not self._app_id:
             return
 
-        print "[kamikaze] data source started"
-        self._timer = start_timer()
-        try:
-            self._times = os.times()
-        except Exception:
-            self._times = None
+        logger.info("data source started")
+        self._timer = _Timer()
+        self._times = os.times()
 
     def stop(self):
         if not self._app_id:
             return
 
-        print "[kamikaze] data source called"
+        logger.info("data source stopped")
         self._timer = None
         self._times = None
 
@@ -50,26 +44,32 @@ class KamikazeDataSource(object):
         new_times = os.times()
         user_time = new_times[0] - self._times[0]
 
-        elapsed_time = self._timer.restart_timer()
+        elapsed_time = self._timer.restart()
 
         utilization = user_time / elapsed_time
-        print "[kamikaze] times = {}; elapsed_time = {}; utilization = {}".format(
-            new_times, elapsed_time, utilization
+        logger.info(
+            "times = %s; elapsed_time = %s; utilization = %s",
+            new_times,
+            elapsed_time,
+            utilization,
         )
 
         while len(self._frames) > 30:
             self._frames.pop(0)
 
-        self._frames.append({
-            "instance_id": os.environ["DYNO"],
-            "process_id": os.getpid(),
-            "type": "web",
-            "start_time": (
-                datetime.datetime.utcnow() - datetime.timedelta(seconds=elapsed_time)
-            ).isoformat(),
-            "end_time": datetime.datetime.utcnow().isoformat(),
-            "usage": 100 * utilization,
-        })
+        self._frames.append(
+            {
+                "instance_id": os.environ["DYNO"],
+                "process_id": os.getpid(),
+                "type": "web",
+                "start_time": (
+                    datetime.datetime.utcnow()
+                    - datetime.timedelta(seconds=elapsed_time)
+                ).isoformat(),
+                "end_time": datetime.datetime.utcnow().isoformat(),
+                "usage": 100 * utilization,
+            }
+        )
 
         self._times = new_times
         self._process_uploads()
@@ -77,7 +77,7 @@ class KamikazeDataSource(object):
 
     def _process_uploads(self):
         try:
-            print "[kamikaze] uploading {} frames".format(len(self._frames))
+            logger.info("uploading %s frames", len(self._frames))
             requests.post(
                 "{}/apps/{}/cpu-usage".format(
                     self._kamikaze_host.rstrip("/"),
@@ -88,39 +88,14 @@ class KamikazeDataSource(object):
                 timeout=5.0,
             ).raise_for_status()
             self._frames = []
-        except:
-            print "[kamikaze] unable to upload frame"
-            traceback.print_exc()
+        except Exception:
+            logger.error("unable to upload frames", exc_info=True)
 
 
 class _Timer(object):
-
     def __init__(self):
-        self._time_started = time.time()
-        self._started = timeit.default_timer()
-        self._stopped = None
+        self._time = timeit.default_timer()
 
-    def time_started(self):
-        return self._time_started
-
-    def stop_timer(self):
-        if self._stopped is None:
-            self._stopped = timeit.default_timer()
-        return self._stopped - self._started
-
-    def restart_timer(self):
-        elapsed_time = self.stop_timer()
-        self._time_started = time.time()
-        self._started = timeit.default_timer()
-        self._stopped = None
-        return elapsed_time
-
-    def elapsed_time(self):
-        if self._stopped is not None:
-            return self._stopped - self._started
-        return timeit.default_timer() - self._started
-
-
-def start_timer():
-    return _Timer()
-
+    def restart(self):
+        _prev_time, self._time = self._time, timeit.default_timer()
+        return self._time - _prev_time
--- setup.py
+++ setup.py
@@ -1,17 +1,4 @@
 import setuptools
 
 
-setuptools.setup(
-    name="geekie-kamikaze-agent",
-    version="1.0.0",
-    url="https://github.com/projetoeureka/kamikaze-agent",
-    maintainer="Geekie",
-    maintainer_email="sherman@geekie.com.br",
-    packages=["kamikaze"],
-    include_package_data=True,
-    zip_safe=False,
-    setup_requires=["setuptools_git==1.0b1"],
-    install_requires=[
-        "requests>=2",
-    ],
-)
+setuptools.setup()
